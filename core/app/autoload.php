bdffac6fcb338a66483799fee1b2e8d90febb812
Aitor <2gdaw02@a224p02.arriagainfo.local>
2022-02-14 08:39:22 +0100

    cambiarAutoload

--- core/app/autoload.php
+++ core/app/autoload.php
@@ -4,12 +4,13 @@
 // esta funcion elimina el hecho de estar agregando los modelos manualmente
 
 
-function __autoload($modelname){
-	if(Model::exists($modelname)){
-		include Model::getFullPath($modelname);
-	} 
-}
-
-
+if(!function_exists('classAutoLoader')){
+        function classAutoLoader($class){
+            $class=strtolower($class);
+            $classFile=$_SERVER['DOCUMENT_ROOT'].'/include/class/'.$class.'.class.php';
+            if(is_file($classFile)&&!class_exists($class)) include $classFile;
+        }
+    }
+    spl_autoload_register('classAutoLoader');
 
 ?>
